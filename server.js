63e880629b289979460376ab3c4842a3d97a5048
Sumit <sumitkumar.kld@gmail.com>
2026-04-23 01:16:32 +0530

    fixed port

--- server.js
+++ server.js
@@ -168,6 +168,8 @@ app.get("/timetable", authMiddleware, async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
